1f217b0095b86f4f4e0fc4b1822f3a34d067d820
Jiayu Li <jiayuliaq@gmail.com>
2023-07-05 22:25:20 +0800

    plot JFP cluster

--- R/analysis.R
+++ R/analysis.R
@@ -63,3 +63,51 @@ copy(dt_covid) %>% #[date > ymd("2021-06-15") & date < ymd("2021-06-15")] %>%
   ylab("Daily cases") +
   xlab("Date in 2021") +
   LJYtheme_basic
+
+dt_jfp_cases <- fread("documents/JFP cluster/JFP_cases_development.csv")
+
+dt_jfp_cases[, date := dmy(date)]
+dt_jfp_cases[, id := seq.int(1:.N), by = date]
+dt_jfp_cases <- dt_jfp_cases[id == 1]
+dt_jfp_cases[, cases_jfp := cases - shift(cases)]
+dt_jfp_cases[1, cases_jfp := 7]
+
+dt_jfp_cases[dt_covid, on = "date"] %>% 
+  .[is.na(cases_jfp), cases_jfp := 0] %>%
+  .[, non_jfp_cases := local_cases_not_residing_in_doms_moh_report- cases_jfp] %>% #[date > ymd("2021-06-15") & date < ymd("2021-06-15")] %>%
+  .[, c(
+    "date",
+    "cases_jfp",
+    "non_jfp_cases",
+    "local_cases_residing_in_dorms_moh_report",
+    # "local_cases_not_residing_in_doms_moh_report",
+    "daily_imported"
+  )] %>%
+  melt(id = "date",
+       variable.name = "case_type",
+       value.name = "case_num") %>%
+  .[case_type == "local_cases_residing_in_dorms_moh_report", case_type := "Dormitory"] %>%
+  .[case_type == "non_jfp_cases", case_type := "Community (not JFP cluster)"] %>%
+  .[case_type == "cases_jfp", case_type := "Community (JFP cluster)"] %>%
+  .[case_type == "daily_imported", case_type := "Imported"] %>%
+  .[, case_type := factor(case_type, levels = c("Dormitory",
+                                                "Imported",
+                                                "Community (not JFP cluster)",
+                                                "Community (JFP cluster)"
+                                                ) )] %>%
+  ggplot() + 
+  geom_col(aes(date, case_num, fill = case_type), position = "stack") +
+  # geom_line(data = dt_covid, aes(date, daily_confirmed)) +
+  geom_vline(xintercept = ymd(c("2021-07-16", "2021-07-02", "2021-08-14") ), linetype ="dashed") +
+  scale_fill_manual("Case type", values = c( "#bdbdbd", "#58B6DC","#BED73B", "#F58A06")) +
+  coord_cartesian(xlim = ymd(c("2021-06-25", "2021-08-19")), ylim = c(0,200)) +
+  # ylab("Daily cases") +
+  # xlab("Date in 2021") +
+  scale_y_continuous("Daily cases", expand = c(0,0)) +
+  scale_x_date("Date in 2021", breaks = ymd(c("2021-07-16", "2021-07-02", "2021-07-31", "2021-08-14")), date_labels = "%b %d") +
+  LJYtheme_basic +
+  guides(fill=guide_legend(nrow=2,byrow=TRUE)) +
+  theme(legend.position = "top")
+
+ggsave("plots/JFP_clusters.pdf", height = 5, width = 8)
+ggsave("plots/JFP_clusters.png", height = 5, width = 6)
